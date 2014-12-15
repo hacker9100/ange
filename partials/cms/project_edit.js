f400eb146680eea7c50bda7c6272644b2a3419c6
hacker9100 <hacker9100@gmail.com>
2014-12-16 07:21:14 +0900

    New changelist

--- partials/cms/project_edit.js
+++ partials/cms/project_edit.js
@@ -109,7 +109,7 @@ define([
 
                         var file = data.FILE;
                         for(var i in file) {
-                            $scope.file = {"name":file.FILE_NM,"size":file[0].FILE_SIZE,"url":UPLOAD.BASE_URL+file[0].PATH+file[0].FILE_ID,"thumbnailUrl":UPLOAD.BASE_URL+file[0].PATH+"thumbnail/"+file[0].FILE_ID,"mediumUrl":UPLOAD.BASE_URL+file[0].PATH+"medium/"+file[0].FILE_ID,"deleteUrl":"http://localhost/serverscript/upload/?file="+file[0].FILE_NM,"deleteType":"DELETE"};
+                            $scope.file = {"name":file[0].FILE_NM,"size":file[0].FILE_SIZE,"url":UPLOAD.BASE_URL+file[0].PATH+file[0].FILE_ID,"thumbnailUrl":UPLOAD.BASE_URL+file[0].PATH+"thumbnail/"+file[0].FILE_ID,"mediumUrl":UPLOAD.BASE_URL+file[0].PATH+"medium/"+file[0].FILE_ID,"deleteUrl":"http://localhost/serverscript/upload/?file="+file[0].FILE_NM,"deleteType":"DELETE"};
                         }
                     })
                     .catch(function(error){dialogs.error('오류', error+'', {size: 'md'});});
@@ -122,6 +122,7 @@ define([
         $scope.click_saveProject = function () {
 //            $scope.item.FILE = $scope.queue;
             $scope.item.FILE = $scope.file;
+            $scope.item.FILE.$destroy = '';
 
             if ($stateParams.id == 0) {
                 $scope.insertItem('cms/project', 'item', $scope.item, false)
