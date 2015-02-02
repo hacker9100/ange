ccd1ccc5d8f65a04853eec88eb608b35424a4f65
Administrator <rlatjdghks9100>
2015-02-02 11:07:34 +0900

    2015 02 02 commit

--- serverscript/services/ange/message.php
+++ serverscript/services/ange/message.php
@@ -1,85 +1,85 @@
 <?php
-    //	header("Content-Type: text/html; charset=UTF-8");
-    //	header("Expires: 0");
-    //	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-    //	header("Cache-Control: no-store, no-cache, must-revalidate");
-    //	header("Cache-Control: pre-check=0, post-check=0, max-age=0");
-    //	header("Pragma: no-cache");
+//	header("Content-Type: text/html; charset=UTF-8");
+//	header("Expires: 0");
+//	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+//	header("Cache-Control: no-store, no-cache, must-revalidate");
+//	header("Cache-Control: pre-check=0, post-check=0, max-age=0");
+//	header("Pragma: no-cache");
 
-    @session_start();
+@session_start();
 
-    @extract($_GET);
-    @extract($_POST);
-    @extract($_SERVER);
+@extract($_GET);
+@extract($_POST);
+@extract($_SERVER);
 
-    date_default_timezone_set('Asia/Seoul');
+date_default_timezone_set('Asia/Seoul');
 
-    include_once($_SERVER['DOCUMENT_ROOT']."/serverscript/classes/ImportClasses.php");
+include_once($_SERVER['DOCUMENT_ROOT']."/serverscript/classes/ImportClasses.php");
 
-    MtUtil::_c("### [START]");
-    MtUtil::_c(print_r($_REQUEST,true));
-    /*
-        if (isset($_REQUEST['_category'])) {
-            $category = explode("/", $_REQUEST['_category']);
+MtUtil::_c("### [START]");
+MtUtil::_c(print_r($_REQUEST,true));
+/*
+    if (isset($_REQUEST['_category'])) {
+        $category = explode("/", $_REQUEST['_category']);
 
-            Util::_c("FUNC[processApi] category : ".print_r($_REQUEST,true));
-            Util::_c("FUNC[processApi] category.cnt : ".count($category));
-        }
-    */
-    $_d = new MtJson();
-
-    if ($_d->connect_db == "") {
-        $_d->failEnd("DB 연결 실패. 관리자에게 문의하세요.");
+        Util::_c("FUNC[processApi] category : ".print_r($_REQUEST,true));
+        Util::_c("FUNC[processApi] category.cnt : ".count($category));
     }
+*/
+$_d = new MtJson();
 
-    if (!isset($_type) || $_type == "") {
-        $_d->failEnd("서버에 문제가 발생했습니다. 작업 유형이 없습니다.");
-    }
+if ($_d->connect_db == "") {
+    $_d->failEnd("DB 연결 실패. 관리자에게 문의하세요.");
+}
 
-    $ip = "";
+if (!isset($_type) || $_type == "") {
+    $_d->failEnd("서버에 문제가 발생했습니다. 작업 유형이 없습니다.");
+}
 
-    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-        $ip = $_SERVER['HTTP_CLIENT_IP'];
-    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    } else {
-        $ip = $_SERVER['REMOTE_ADDR'];
-    }
+$ip = "";
 
-    switch ($_method) {
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
 
-        case "GET":
-            if($_type == 'item'){
+switch ($_method) {
 
-                $sql = "SELECT  NO, TO_ID, TO_NM, FROM_ID, FROM_NM, BODY, DATE_FORMAT(REG_DT, '%Y-%m-%d') AS REG_DT, CHECK_FL
+    case "GET":
+        if($_type == 'item'){
+
+            $sql = "SELECT  NO, TO_ID, TO_NM, FROM_ID, FROM_NM, BODY, DATE_FORMAT(REG_DT, '%Y-%m-%d') AS REG_DT, CHECK_FL
                      FROM (
                                 SELECT NO, TO_ID, TO_NM, FROM_ID, FROM_NM, BODY, REG_DT, CHECK_FL
                                 FROM ANGE_MESSAGE
                                 WHERE NO = ".$_key."
                      )  A";
 
-                $data = $_d->sql_query($sql);
-                if ($_d->mysql_errno > 0) {
-                    $_d->failEnd("조회실패입니다:".$_d->mysql_error);
-                } else {
-                    $result = $_d->sql_query($sql);
-                    $data = $_d->sql_fetch_array($result);
-                    $_d->dataEnd2($data);
-                }
-            } else if($_type == 'list'){
+            $data = $_d->sql_query($sql);
+            if ($_d->mysql_errno > 0) {
+                $_d->failEnd("조회실패입니다:".$_d->mysql_error);
+            } else {
+                $result = $_d->sql_query($sql);
+                $data = $_d->sql_fetch_array($result);
+                $_d->dataEnd2($data);
+            }
+        } else if($_type == 'list'){
 
-                // 검색조건 추가
- /*               if (isset($_search[REG_UID]) && $_search[REG_UID] != "") {
-                    $search_where .= "AND AMS.USER_ID = '".$_SESSION['uid']."'";
-                }*/
+            // 검색조건 추가
+            /*               if (isset($_search[REG_UID]) && $_search[REG_UID] != "") {
+                               $search_where .= "AND AMS.USER_ID = '".$_SESSION['uid']."'";
+                           }*/
 
-                $limit = "";
+            $limit = "";
 
-                if (isset($_page)) {
-                    $limit .= "LIMIT ".($_page[NO] * $_page[SIZE]).", ".$_page[SIZE];
-                }
+            if (isset($_page)) {
+                $limit .= "LIMIT ".($_page[NO] * $_page[SIZE]).", ".$_page[SIZE];
+            }
 
-                $sql = " SELECT STATUS, TO_CNT, FROM_CNT, NO, TO_ID, TO_NM, FROM_ID, FROM_NM, BODY, CHECK_FL,
+            $sql = " SELECT STATUS, TO_CNT, FROM_CNT, NO, TO_ID, TO_NM, FROM_ID, FROM_NM, BODY, CHECK_FL,
                               DATE_FORMAT(REG_DT, '%Y-%m-%d') AS REG_DT,
                                  TO_CNT+FROM_CNT AS TOTAL_CNT
                         FROM
@@ -118,19 +118,60 @@
                         ".$limit."
                 ";
 
-                $data = $_d->sql_query($sql);
-                if($_d->mysql_errno > 0){
-                    $_d->failEnd("조회실패입니다:".$_d->mysql_error);
-                }else{
-                    $_d->dataEnd($sql);
-                }
+            $data = $_d->sql_query($sql);
+            if($_d->mysql_errno > 0){
+                $_d->failEnd("조회실패입니다:".$_d->mysql_error);
+            }else{
+                $_d->dataEnd($sql);
+            }
+        }else if($_type == 'searchuserlist'){
+
+            $search_where = "";
+
+            // 검색조건 추가
+            if (isset($_search[TO_ID]) && $_search[TO_ID] != "") {
+                $search_where .= "AND CU.USER_ID LIKE '%".$_search[TO_ID]."%'";
+            } else if (isset($_search[TO_NM]) && $_search[TO_NM] != "") {
+                $search_where .= "AND CU.NICK_NM LIKE '%".$_search[TO_NM]."%'";
             }
 
-            break;
+            $limit = "";
 
-        case "POST":
-            if ($_type == "item") {
-                $sql = "INSERT INTO ANGE_MESSAGE
+            if (isset($_page)) {
+                $limit .= "LIMIT ".($_page[NO] * $_page[SIZE]).", ".$_page[SIZE];
+            }
+
+            $sql = " SELECT USER_ID, NICK_NM, TOTAL_COUNT
+                        FROM
+                        (
+                           SELECT USER_ID, NICK_NM
+                           FROM COM_USER CU
+                           WHERE 1=1
+                              ".$search_where."
+                        ) AS DATA,
+                        (
+                            SELECT
+                                COUNT(*) AS TOTAL_COUNT
+                            FROM COM_USER CU
+                            WHERE 1=1
+                              ".$search_where."
+                        ) FROM_CNT
+                        ".$limit."
+                ";
+
+            $data = $_d->sql_query($sql);
+            if($_d->mysql_errno > 0){
+                $_d->failEnd("조회실패입니다:".$_d->mysql_error);
+            }else{
+                $_d->dataEnd($sql);
+            }
+        }
+
+        break;
+
+    case "POST":
+        if ($_type == "item") {
+            $sql = "INSERT INTO ANGE_MESSAGE
                         (
                             TO_ID,
                             TO_NM,
@@ -149,68 +190,68 @@
                             , 'N'
                         )";
 
-                /*".$_model[SORT_IDX]."*/
+            /*".$_model[SORT_IDX]."*/
 
-                $_d->sql_query($sql);
-                $no = $_d->mysql_insert_id;
+            $_d->sql_query($sql);
+            $no = $_d->mysql_insert_id;
 
-                if ($_d->mysql_errno > 0) {
-                    $_d->failEnd("등록실패입니다:".$_d->mysql_error);
-                } else {
-                    $_d->succEnd($no);
+            if ($_d->mysql_errno > 0) {
+                $_d->failEnd("등록실패입니다:".$_d->mysql_error);
+            } else {
+                $_d->succEnd($no);
+            }
+        } else if ($_type == "admin") {
+            $search_where = "";
+
+            $err = 0;
+            $msg = "";
+
+            $_d->sql_beginTransaction();
+
+            if ($_model[CHECKED] == "C") {
+                if (isset($_model[USER_ID_LIST])) {
+                    $in_str = "";
+                    $in_size = sizeof($_model[USER_ID_LIST]);
+                    for ($i=0; $i< $in_size; $i++) {
+                        $in_str .= "'".trim($_model[USER_ID_LIST][$i])."'";
+                        if ($in_size - 1 != $i) $in_str .= ",";
+                    }
+
+                    $search_where = "AND USER_ID IN (".$in_str.") ";
                 }
-            } else if ($_type == "admin") {
-                $search_where = "";
-
-                $err = 0;
-                $msg = "";
-
-                $_d->sql_beginTransaction();
-
-                if ($_model[CHECKED] == "C") {
-                    if (isset($_model[USER_ID_LIST])) {
-                        $in_str = "";
-                        $in_size = sizeof($_model[USER_ID_LIST]);
-                        for ($i=0; $i< $in_size; $i++) {
-                            $in_str .= "'".trim($_model[USER_ID_LIST][$i])."'";
-                            if ($in_size - 1 != $i) $in_str .= ",";
+            } else {
+                if ((isset($_model[CONDITION]) && $_model[CONDITION] != "") && (isset($_model[KEYWORD]) && $_model[KEYWORD] != "")) {
+                    if ($_model[CONDITION][value] == "USER_NM" || $_model[CONDITION][value] == "USER_ID" || $_model[CONDITION][value] == "NICK_NM") {
+                        $arr_keywords = explode(",", $_model[KEYWORD]);
+                        $in_condition = "";
+                        for ($i=0; $i< sizeof($arr_keywords); $i++) {
+                            $in_condition .= "'".trim($arr_keywords[$i])."'";
+                            if (sizeof($arr_keywords) - 1 != $i) $in_condition .= ",";
                         }
 
-                        $search_where = "AND USER_ID IN (".$in_str.") ";
-                    }
-                } else {
-                    if ((isset($_model[CONDITION]) && $_model[CONDITION] != "") && (isset($_model[KEYWORD]) && $_model[KEYWORD] != "")) {
-                        if ($_model[CONDITION][value] == "USER_NM" || $_model[CONDITION][value] == "USER_ID" || $_model[CONDITION][value] == "NICK_NM") {
-                            $arr_keywords = explode(",", $_model[KEYWORD]);
-                            $in_condition = "";
-                            for ($i=0; $i< sizeof($arr_keywords); $i++) {
-                                $in_condition .= "'".trim($arr_keywords[$i])."'";
-                                if (sizeof($arr_keywords) - 1 != $i) $in_condition .= ",";
-                            }
-
-                            $search_where .= "AND ".$_model[CONDITION][value]." IN (".$in_condition.") ";
-                        } else if ($_model[CONDITION][value] == "PHONE") {
-                            $search_where .= "AND ( PHONE_1 LIKE '%".$_model[KEYWORD]."%' OR PHONE_2 LIKE '%".$_model[KEYWORD]."%' ) ";
-                        } else {
-                            $search_where .= "AND ".$_model[CONDITION][value]." LIKE '%".$_model[KEYWORD]."%' ";
-                        }
-                    }
-                    if (isset($_model[TYPE]) && $_model[TYPE] != "") {
-
-                        $in_type = "";
-                        for ($i=0; $i< count($_model[TYPE]); $i++) {
-                            $in_type .= "'".$_model[TYPE][$i]."'";
-                            if (count($_model[TYPE]) - 1 != $i) $in_type .= ",";
-                        }
-
-                        $search_where .= "AND USER_GB IN (".$in_type.") ";
-                    }
-                    if (isset($_model[STATUS]) && $_model[STATUS] != "" && $_model[STATUS][value] != "A") {
-                        $search_where .= "AND USER_ST  = '".$_model[STATUS][value]."' ";
+                        $search_where .= "AND ".$_model[CONDITION][value]." IN (".$in_condition.") ";
+                    } else if ($_model[CONDITION][value] == "PHONE") {
+                        $search_where .= "AND ( PHONE_1 LIKE '%".$_model[KEYWORD]."%' OR PHONE_2 LIKE '%".$_model[KEYWORD]."%' ) ";
+                    } else {
+                        $search_where .= "AND ".$_model[CONDITION][value]." LIKE '%".$_model[KEYWORD]."%' ";
                     }
                 }
+                if (isset($_model[TYPE]) && $_model[TYPE] != "") {
 
-                $sql = "SELECT USER_ID, NICK_NM
+                    $in_type = "";
+                    for ($i=0; $i< count($_model[TYPE]); $i++) {
+                        $in_type .= "'".$_model[TYPE][$i]."'";
+                        if (count($_model[TYPE]) - 1 != $i) $in_type .= ",";
+                    }
+
+                    $search_where .= "AND USER_GB IN (".$in_type.") ";
+                }
+                if (isset($_model[STATUS]) && $_model[STATUS] != "" && $_model[STATUS][value] != "A") {
+                    $search_where .= "AND USER_ST  = '".$_model[STATUS][value]."' ";
+                }
+            }
+
+            $sql = "SELECT USER_ID, NICK_NM
                         FROM
                             COM_USER
                         WHERE
@@ -218,9 +259,9 @@
                             ".$search_where."
                         ";
 
-                $result = $_d->sql_query($sql,true);
-                for ($i=0; $row=$_d->sql_fetch_array($result); $i++) {
-                    $sql = "INSERT INTO ANGE_MESSAGE
+            $result = $_d->sql_query($sql,true);
+            for ($i=0; $row=$_d->sql_fetch_array($result); $i++) {
+                $sql = "INSERT INTO ANGE_MESSAGE
                         (
                             TO_ID,
                             TO_NM,
@@ -239,81 +280,81 @@
                             , 'N'
                         )";
 
-                    $_d->sql_query($sql);
+                $_d->sql_query($sql);
 
-                    if ($_d->mysql_errno > 0) {
-                        $err++;
-                        $msg = $_d->mysql_error;
-                    }
-                }
-
-                if ($err > 0) {
-                    $_d->sql_rollback();
-                    $_d->failEnd("등록실패입니다:".$msg);
-                } else {
-                    $_d->sql_commit();
-                    $_d->succEnd($no);
+                if ($_d->mysql_errno > 0) {
+                    $err++;
+                    $msg = $_d->mysql_error;
                 }
             }
 
+            if ($err > 0) {
+                $_d->sql_rollback();
+                $_d->failEnd("등록실패입니다:".$msg);
+            } else {
+                $_d->sql_commit();
+                $_d->succEnd($no);
+            }
+        }
 
-            break;
 
-        case "PUT":
+        break;
 
-            MtUtil::_c("### [POST_DATA] ".json_encode(file_get_contents("php://input"),true));
+    case "PUT":
 
-            if($_type == 'check'){
+        MtUtil::_c("### [POST_DATA] ".json_encode(file_get_contents("php://input"),true));
 
-                $sql = "UPDATE ANGE_MESSAGE SET
+        if($_type == 'check'){
+
+            $sql = "UPDATE ANGE_MESSAGE SET
                             CHECK_FL = 'Y'
                      WHERE NO = '".$_key."'
                         ";
-
-                $_d->sql_query($sql);
-                $no = $_d->mysql_insert_id;
-
-                if ($_d->mysql_errno > 0) {
-                    $_d->failEnd("수정실패입니다:".$_d->mysql_error);
-                } else {
-                    $_d->succEnd($no);
-                }
-
-            } else{
-
-                $sql = "UPDATE ANGE_MESSAGE SET
-                            TO_ID = '".$_model[TO_ID]."'
-                            , TO_NM = '".$_model[TO_NM]."'
-                     WHERE NO = '".$_key."'
-                        ";
-
-                $_d->sql_query($sql);
-                $no = $_d->mysql_insert_id;
-
-                if ($_d->mysql_errno > 0) {
-                    $_d->failEnd("수정실패입니다:".$_d->mysql_error);
-                } else {
-                    $_d->succEnd($no);
-                }
-            }
-
-            break;
-
-        case "DELETE":
-            if (!isset($_key) || $_key == '') {
-                $_d->failEnd("삭제실패입니다:"."KEY가 누락되었습니다.");
-            }
-
-            $sql = "DELETE FROM ANGE_MESSAGE WHERE NO = ".$_key;
 
             $_d->sql_query($sql);
             $no = $_d->mysql_insert_id;
 
             if ($_d->mysql_errno > 0) {
-                $_d->failEnd("삭제실패입니다:".$_d->mysql_error);
+                $_d->failEnd("수정실패입니다:".$_d->mysql_error);
             } else {
                 $_d->succEnd($no);
             }
 
-            break;
-    }
+        } else{
+
+            $sql = "UPDATE ANGE_MESSAGE SET
+                            TO_ID = '".$_model[TO_ID]."'
+                            , TO_NM = '".$_model[TO_NM]."'
+                     WHERE NO = '".$_key."'
+                        ";
+
+            $_d->sql_query($sql);
+            $no = $_d->mysql_insert_id;
+
+            if ($_d->mysql_errno > 0) {
+                $_d->failEnd("수정실패입니다:".$_d->mysql_error);
+            } else {
+                $_d->succEnd($no);
+            }
+        }
+
+        break;
+
+    case "DELETE":
+        if (!isset($_key) || $_key == '') {
+            $_d->failEnd("삭제실패입니다:"."KEY가 누락되었습니다.");
+        }
+
+        $sql = "DELETE FROM ANGE_MESSAGE WHERE NO = ".$_key;
+
+        $_d->sql_query($sql);
+        $no = $_d->mysql_insert_id;
+
+        if ($_d->mysql_errno > 0) {
+            $_d->failEnd("삭제실패입니다:".$_d->mysql_error);
+        } else {
+            $_d->succEnd($no);
+        }
+
+        break;
+}
