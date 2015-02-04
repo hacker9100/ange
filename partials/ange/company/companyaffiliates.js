b832f9d924e0d5f503caf117f0cf62b75efb8190
Administrator <rlatjdghks9100>
2015-02-04 19:16:42 +0900

    2015 02 04 commit

--- partials/ange/company/companyaffiliates.js
+++ partials/ange/company/companyaffiliates.js
@@ -13,12 +13,31 @@ define([
     // 사용할 서비스를 주입
     controllers.controller('companyaffiliates', ['$rootScope', '$scope', '$window', '$location', 'dialogs', 'UPLOAD', function ($rootScope, $scope, $window, $location, dialogs, UPLOAD) {
 
+        $scope.item = {};
         /********** 초기화 **********/
         $scope.init = function () {
 
+            $scope.item.COMPANY_GB = 'MAGAZINE';
         };
 
         /********** 이벤트 **********/
+        $scope.click_comp = function (){
+
+            $scope.item.COMPANY_PHONE1 = $scope.item.PHONE1_1+$scope.item.PHONE1_2+$scope.item.PHONE1_3;
+            $scope.item.COMPANY_PHONE2 = $scope.item.PHONE2_1+$scope.item.PHONE2_2+$scope.item.PHONE2_3;
+            $scope.item.COMPANY_EMAIL = $scope.item.EMAIL_ID+'@'+$scope.item.EMAIL_ADDRESS;
+
+            $scope.insertItem('ange/company', 'item', $scope.item, false)
+                .then(function(){
+
+                    dialogs.notify('알림', '정상적으로 등록되었습니다.', {size: 'md'});
+
+                    $location.url("main");
+                })
+                .catch(function(error){dialogs.error('오류', error+'', {size: 'md'});});
+        }
+
+
 
         $scope.init();
 	}]);
