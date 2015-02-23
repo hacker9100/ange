2025c257f5e85dde631d7c8f59e0a63214968adf
Administrator <rlatjdghks9100>
2015-02-23 12:24:02 +0900

    2015 02 23 commit

--- partials/ange/moms/momsexperience-view.js
+++ partials/ange/moms/momsexperience-view.js
@@ -363,10 +363,18 @@ define([
                 answer.push($scope.item.QUE_SELECT_ANSWER); // 객관식
                 $rootScope.jsontext2 = new Array();
                 //
-                for(var i=0; i<answer.length; i++){
-                    var index = parseInt(i+1);
-                    $rootScope.jsontext2[i] = '"'+index+'":"'+ answer[i]+'"';
-                }
+//                for(var i=0; i<answer.length; i++){
+//                    var index = parseInt(i+1);
+//                    $rootScope.jsontext2[i] = '"'+index+'":"'+ answer[i]+'"';
+//                }
+                $("input[name='index[]'").each(function(index, element) {
+                    //$scope.item.QUE_SHORT_ANSWER = $(element).val();
+                    //$rootScope.jsontext2[$(element).val()] = '"'+$(element).val()+'":"'+ answer[index]+'"';
+                    $rootScope.jsontext2[index] = '"'+$(element).val()+'":"'+ answer[index]+'"'; //[index] [$(element).val()]
+                })
+
+                $scope.item.ANSWER = '{'+$rootScope.jsontext2+'}';
+                console.log($scope.item.ANSWER);
 
                 $scope.item.ANSWER = '{'+$rootScope.jsontext2+'}';
                 console.log($scope.item.ANSWER);
