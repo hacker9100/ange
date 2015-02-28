e061315e394f46a3e6267ed8679b97fac6ac3357
Administrator <rlatjdghks9100>
2015-02-28 15:34:46 +0900

    New changelist

--- partials/ange/moms/momsboard-list.js
+++ partials/ange/moms/momsboard-list.js
@@ -61,7 +61,7 @@ define([
         // 게시판 목록 조회
         $scope.getPeopleBoardList = function () {
 
-            $scope.getList('ange/winner', 'list', {NO: $scope.PAGE_NO - 1, SIZE: $scope.PAGE_SIZE}, $scope.search, true)
+            $scope.getList('com/webboard', 'list', {NO: $scope.PAGE_NO - 1, SIZE: $scope.PAGE_SIZE}, $scope.search, true)
                 .then(function(data){
                     var total_cnt = data[0].TOTAL_COUNT;
                     $scope.TOTAL_COUNT = total_cnt;
