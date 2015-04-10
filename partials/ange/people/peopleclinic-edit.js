69f3bcd275253af166f7d6c7004fcc849d5815f0
hacker9100 <hacker9100@gmail.com>
2015-04-10 10:13:09 +0900

    20150410 hwan

--- partials/ange/people/peopleclinic-edit.js
+++ partials/ange/people/peopleclinic-edit.js
@@ -123,11 +123,11 @@ define([
         $scope.init = function(session) {
 
             if ($stateParams.menu == 'childdevelop') {
-                $scope.item.BODY = "<span style='color: #0000ff'>아이 만나이 :</span> <br/><span style='color: #0000ff'>아이 성별:</span> <hr></hr>";
+                $scope.item.BODY = "<span style='color: #0000ff'>아이 만나이 :</span> <br/><span style='color: #0000ff'>아이 성별:</span> <br/><br/>";
             } else if($stateParams.menu == 'chlidoriental') {
-                $scope.item.BODY = "<span style='color: #0000ff'>아이 만나이 :</span> <br/><span style='color: #0000ff'>아이 성별:</span> <hr></hr>";
+                $scope.item.BODY = "<span style='color: #0000ff'>아이 만나이 :</span> <br/><span style='color: #0000ff'>아이 성별:</span> <br/><br/>";
             } else if($stateParams.menu == 'obstetrics') {
-                $scope.item.BODY = "<span style='color: #0000ff'>문의 대상 : <span style='color: #808080'>예) 아이, 본인</span></span> <br/><span style='color: #0000ff'>문의 나이:</span> <br/>---------------------------------------------------------------------------------------------------------------------------------------------";
+                $scope.item.BODY = "<span style='color: #0000ff'>문의 대상 : <span style='color: #808080'>예) 아이, 본인</span></span> <br/><span style='color: #0000ff'>문의 나이:</span> <br/><br/>";
             } else if($stateParams.menu == 'momshealth') {
                 $scope.item.BODY = "";
             } else if($stateParams.menu == 'financial') {
